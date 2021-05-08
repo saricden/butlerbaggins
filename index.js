5160e299d833dce6097bcb59f04c0e5dd6d3fdcb
Kirk Morris <worker.kirk@gmail.com>
2021-05-08 14:55:35 -0700

    Adds trivia sort of but lazy.

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const Discord = require('discord.js');
 const fetch = require('node-fetch');
 const client = new Discord.Client();
 
+let qaQ = "";
+let qaAns = "";
+
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -19,20 +22,37 @@ client.on('message', msg => {
   if (msg.author.bot) return;
   let isAtMe = false;
   const isChuck = (msg.content.toLowerCase().includes('chuck'));
+  const isTrivia = (msg.content.toLowerCase().includes('trivia'));
+  const isAns = (msg.content.toLowerCase().includes('trivia') && msg.content.toLowerCase().includes('answer'));
 
   msg.mentions.users.forEach((mention) => {
     if (mention.bot && mention.username === 'butlerbaggins') isAtMe = true;
   });
-
   if (isAtMe) {
     console.log(msg);
-    if (isChuck) {
+    if (isAns) {
+      msg.reply(`The answer was ${qaAns}.`);
+    }
+    else if (isTrivia) {
+      fetch('https://jservice.io/api/random')
+      .then(res => res.json())
+      .then((json) => {
+        const [qa] = json;
+        const {question, answer, value, category} = qa;
+        const {title} = category;
+
+        qaQ = question.toLowerCase();
+        qaAns = answer.toLowerCase();
+        msg.reply(`${title} for ${value}... ${question}?`);
+      });
+    }
+    else if (isChuck) {
       fetch('https://api.chucknorris.io/jokes/random')
-        .then(res => res.json())
-        .then((json) => {
-          const {value} = json;
-          msg.reply(value);
-        });
+      .then(res => res.json())
+      .then((json) => {
+        const {value} = json;
+        msg.reply(value);
+      });
     }
     else {
       fetch('https://the-one-api.dev/v2/character/5cd99d4bde30eff6ebccfc38/quote', {
